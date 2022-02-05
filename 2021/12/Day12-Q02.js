8ef016ebabd9ac4e0f375c12dbe4eece89e9cb75
Krishna Sharma <krishnasharma1495@gmail.com>
2022-02-05 08:32:15 +0530

    WIP 2021 day 12 Q2

--- 2021/12/Day12-Q02.js
+++ 2021/12/Day12-Q02.js
@@ -1,66 +1,6 @@
-let puzzleInput = [
-  "vp-BY",
-  "ui-oo",
-  "kk-IY",
-  "ij-vp",
-  "oo-start",
-  "SP-ij",
-  "kg-uj",
-  "ij-UH",
-  "SP-end",
-  "oo-IY",
-  "SP-kk",
-  "SP-vp",
-  "ui-ij",
-  "UH-ui",
-  "ij-IY",
-  "start-ui",
-  "IY-ui",
-  "uj-ui",
-  "kk-oo",
-  "IY-start",
-  "end-vp",
-  "uj-UH",
-  "ij-kk",
-  "UH-end",
-  "UH-kk",
-];
+const puzzleInput = require("./puzzleInputs");
 
-puzzleInput2 = [
-  "dc-end",
-  "HN-start",
-  "start-kj",
-  "dc-start",
-  "dc-HN",
-  "LN-dc",
-  "HN-end",
-  "kj-sa",
-  "kj-HN",
-  "kj-dc",
-];
-
-puzzleInput3 = [
-  "fs-end",
-  "he-DX",
-  "fs-he",
-  "start-DX",
-  "pj-DX",
-  "end-zg",
-  "zg-sl",
-  "zg-pj",
-  "pj-he",
-  "RW-he",
-  "fs-DX",
-  "pj-RW",
-  "zg-RW",
-  "start-pj",
-  "he-WI",
-  "zg-he",
-  "pj-fs",
-  "start-RW",
-];
-
-let connections = {};
+const connections = {};
 puzzleInput.forEach((connection) => {
   const [cave1, cave2] = connection.split("-");
   if (connections[cave1] === undefined) connections[cave1] = [];
@@ -69,64 +9,75 @@ puzzleInput.forEach((connection) => {
   if (!connections[cave2].includes(cave1)) connections[cave2].push(cave1);
 });
 
-const findPathCombinations = (path) => {
-  const lastCave = path[path.length - 1];
-  if (lastCave === "end") return null;
+const caves = {};
+Object.keys(connections).forEach((caveName) => {
+  caves[caveName] = {
+    name: caveName,
+    isSmall: caveName === caveName.toLowerCase(),
+  };
+});
 
-  let newPaths = [];
-  connections[lastCave].forEach((nextCave) => {
-    if (nextCave.toUpperCase() === nextCave) {
-      newPaths.push([...path, nextCave]);
-    } else {
-      if (path.includes(nextCave)) {
-        if (nextCave !== "start" && nextCave !== "end") {
-          const pathConnections = path.reduce(
-            (prev, cur) =>
-              Object.assign(prev, {
-                [cur]:
-                  prev[cur] === undefined || cur.toUpperCase() === cur
-                    ? 1
-                    : prev[cur] + 1,
-              }),
-            { [nextCave]: 1 }
-          );
-          if (
-            Object.values(pathConnections).filter((count) => count >= 2)
-              .length <= 1 &&
-            Object.values(pathConnections).filter((count) => count >= 3)
-              .length < 1 &&
-            Object.values(pathConnections).length < 100
-          ) {
-            newPaths.push([...path, nextCave]);
-          }
-        }
-      } else {
-        newPaths.push([...path, nextCave]);
-      }
-    }
-  });
-  return newPaths;
-};
+const isValidPath = (path) => {
+  let isValid = true;
+  const caveCounts = {};
+  let smallCaveVisitedTwice = false;
+  const cavesInPath = path.split(",");
+  if (cavesInPath.length > 100) {
+    return false;
+  }
+  for (caveName of cavesInPath) {
+    caveCounts[caveName] = caveCounts[caveName] ? caveCounts[caveName] + 1 : 1;
 
-let uniquePaths = ["start"];
-let flag;
-do {
-  flag = false;
-  for (let i = 0; i < uniquePaths.length; i++) {
-    if (!uniquePaths[i].endsWith("-end")) {
-      const newUniquePaths = findPathCombinations(uniquePaths[i].split("-"))
-        .map((path) => path.join("-"))
-        .filter((path) => !uniquePaths.includes(path));
-
-      if (newUniquePaths.length) {
-        uniquePaths = [...uniquePaths, ...newUniquePaths];
-        flag = true;
+    if (caves[caveName].isSmall) {
+      if (caveCounts[caveName] > 2) {
+        // a small cave was visited more than twice
+        isValid = false;
         break;
+      } else if (caveCounts[caveName] === 2) {
+        if (
+          caveName === "start" ||
+          caveName === "end" ||
+          smallCaveVisitedTwice
+        ) {
+          // start or end cave was visited twice
+          // or more than one small caves were visited twice
+          isValid = false;
+          break;
+        } else {
+          // a small cave was visited twice
+          smallCaveVisitedTwice = true;
+        }
       }
     }
   }
-} while (flag);
 
-uniquePaths = uniquePaths.sort().filter((path) => path.endsWith("-end"));
+  return isValid && path.startsWith("start");
+};
 
+const uniquePaths = [];
+const partialPaths = [];
+const seekNewPath = (currentPath) => {
+  if (currentPath.endsWith(",end")) {
+    if (!uniquePaths.includes(currentPath)) {
+      uniquePaths.push(currentPath);
+    }
+  } else {
+    const lastCave = currentPath.split(",").pop();
+    for (nextCave of connections[lastCave]) {
+      const nextPath = currentPath + "," + nextCave;
+      if (!partialPaths.includes(nextPath)) {
+        partialPaths.push(nextPath);
+        if (isValidPath(nextPath)) {
+          seekNewPath(nextPath);
+        }
+      }
+    }
+  }
+};
+
+console.log(connections);
+setInterval(() => {
+  console.log(partialPaths.length, uniquePaths.length);
+}, 1000);
+seekNewPath("start");
 console.log(uniquePaths.length);
---
+++ 2021/12/puzzleInputs.js
@@ -0,0 +1,73 @@
+const puzzleInput1 = [
+  "vp-BY",
+  "ui-oo",
+  "kk-IY",
+  "ij-vp",
+  "oo-start",
+  "SP-ij",
+  "kg-uj",
+  "ij-UH",
+  "SP-end",
+  "oo-IY",
+  "SP-kk",
+  "SP-vp",
+  "ui-ij",
+  "UH-ui",
+  "ij-IY",
+  "start-ui",
+  "IY-ui",
+  "uj-ui",
+  "kk-oo",
+  "IY-start",
+  "end-vp",
+  "uj-UH",
+  "ij-kk",
+  "UH-end",
+  "UH-kk",
+];
+
+const puzzleInput2 = [
+  "dc-end",
+  "HN-start",
+  "start-kj",
+  "dc-start",
+  "dc-HN",
+  "LN-dc",
+  "HN-end",
+  "kj-sa",
+  "kj-HN",
+  "kj-dc",
+];
+
+const puzzleInput3 = [
+  "fs-end",
+  "he-DX",
+  "fs-he",
+  "start-DX",
+  "pj-DX",
+  "end-zg",
+  "zg-sl",
+  "zg-pj",
+  "pj-he",
+  "RW-he",
+  "fs-DX",
+  "pj-RW",
+  "zg-RW",
+  "start-pj",
+  "he-WI",
+  "zg-he",
+  "pj-fs",
+  "start-RW",
+];
+
+const puzzleInput4 = [
+  "start-A",
+  "start-b",
+  "A-c",
+  "A-b",
+  "b-d",
+  "A-end",
+  "b-end",
+];
+
+module.exports = puzzleInput4;
